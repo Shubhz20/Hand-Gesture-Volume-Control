be805b97583008622d9e7c0365dee876ceb4ad00
Harshit Agrawal <harshitagrawal.dev@gmail.com>
2026-04-05 19:29:12 +0530

    Updated streamlit_app.py with user changes to force pip install

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import os
 import sys
 import subprocess
-try:
-    import cv2
-except ImportError:
-    pass
+# try:
+#     import cv2
+# except ImportError:
+#     pass
 # Force headless mode to prevent Streamlit Cloud EGL context crashes
 subprocess.call([sys.executable, "-m", "pip", "uninstall", "-y", "opencv-python", "opencv-contrib-python"])
 subprocess.call([sys.executable, "-m", "pip", "install", "--no-cache-dir", "opencv-python-headless", "opencv-contrib-python-headless"])
