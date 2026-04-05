51a195890cd187e0860206b757b8340b2525edf1
Harshit Agrawal <harshitagrawal.dev@gmail.com>
2026-04-05 16:26:46 +0530

    Use libglib2.0-dev to bypass broken apt dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
-import os
-import sys
-import subprocess
-
-# Hack to fix Streamlit Cloud missing libGL / libgthread due to mediapipe downloading opencv-contrib-python
-try:
-    import cv2
-except ImportError:
-    subprocess.call([sys.executable, "-m", "pip", "uninstall", "-y", "opencv-python", "opencv-contrib-python"])
-    subprocess.call([sys.executable, "-m", "pip", "install", "--no-cache-dir", "opencv-python-headless", "opencv-contrib-python-headless"])
-    import cv2
-
+import cv2
 import mediapipe as mp
 import numpy as np
 import streamlit as st
